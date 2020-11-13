5c34114bb95a53cccbb11ed6feb62c8244266360
Amin Frouja <aminfrouja7@gmail.com>
2020-11-14 00:00:15 +0100

    challenge II done

--- src/C_file_hello_ESPL.c
+++ src/C_file_hello_ESPL.c
@@ -1,6 +1,20 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<espl_lib.h>
 
-int main(void) {
-    printf ("Hello ESPL\n");
-    return 0;
-}
+int main(){
+    int n =0;
+    int p =1;
+    char a;
+
+    
+    while (p==1){
+        printf("give me a number please : \n");
+        scanf ("%d",&n);
+        printf("%s\n" , num_to_words(n));
+        printf("type r to Replay or another letter to Exit : \n");
+        getchar();
+        scanf("%c",&a);
+        if (a!='r') p = 0;
+
+    }
+} 
